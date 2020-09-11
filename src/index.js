02ddde4ed85f42d63b63fd876785725b2ccb4676
wardou2 <ward92@gmail.com>
2020-09-11 13:25:05 -0400

    Add ESLint and Prettier and update packages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Tone from 'tone';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Tone from "tone";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
