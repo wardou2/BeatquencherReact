bd38aa51b1a4fb4d205ab68df4f64bc817045ed6
wardou2 <ward92@gmail.com>
2020-09-22 12:51:57 -0400

    Redesign Landing Pagee
    Still work to be done

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { Button, Segment } from "semantic-ui-react";
+import { Button, Segment, Grid, Divider } from "semantic-ui-react";
 import NewUser from "./NewUser";
 
 const Landing = ({ getUser, startDemo }) => {
     return (
         <div>
             <br></br>
-            <Segment>
-                <Button.Group>
-                    <Button onClick={startDemo}>Demo without Logging in</Button>
-                    <Button.Or />
-                    <NewUser getUser={getUser} />
-                </Button.Group>
+            <Segment placeholder>
+                <Grid columns={2} stackable textAlign="center">
+                    <Divider vertical>OR</Divider>
+                    <Grid.Row verticalAlign="middle">
+                        <Grid.Column>
+                            <Button onClick={startDemo}>
+                                Demo without Logging in
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <NewUser getUser={getUser} />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </Segment>
         </div>
     );
