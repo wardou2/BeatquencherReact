dffe82b890def032f41b68cbbd866c3c6ad94cca
wardou2 <ward92@gmail.com>
2020-10-01 15:53:36 -0400

    Try different server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static(`${__dirname}/`));
-app.listen(process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(PORT);
